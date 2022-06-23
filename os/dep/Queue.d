829aea7235e0726d33dd6c5ec55b6383fd058b79
Björn <bjornbjorn@hotmail.de>
2022-06-23 16:13:25 +0200

    build stuff

--- os/dep/Queue.d
+++ os/dep/Queue.d
@@ -3,4 +3,6 @@ build/Queue.o: lib/Queue.cc lib/Queue.h lib/Chain.h kernel/Globals.h \
  kernel/Allocator.h kernel/allocator/LinkedListAllocator.h \
  devices/PCSPK.h kernel/IOport.h devices/CGA_Stream.h devices/CGA.h \
  lib/OutStream.h lib/StringBuffer.h devices/Keyboard.h devices/Key.h \
- kernel/interrupts/ISR.h kernel/interrupts/IntDispatcher.h
+ kernel/interrupts/ISR.h kernel/interrupts/IntDispatcher.h \
+ kernel/threads/Scheduler.h kernel/threads/Dispatch.h \
+ kernel/threads/Thread.h kernel/threads/ThreadState.h
